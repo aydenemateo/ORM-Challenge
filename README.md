65dc0325dbd8a2f5ea8fabb91fe751209cf14c5f
amateolo <aydenemateo@gmail.com>
2023-06-11 15:24:49 -0400

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # ORM-Challenge
+
+## Description
+
+I worked with the back-end code of an e-commerce website, modifying it to use Express.js API to utilize Squelize to interact with a MySQL database.
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Credits](#credits)
+* [License](#license)
+* [Questions](#questions)
+
+
+## Installation
+
+Open VS Code and open the terminal. Then enter MySQL to source the schema and exit the schema. Run NPM RUN SEED to seed the database and then run NPM start to initiate listening to you local server. Use Insomnia to interact with th categories, products or tags routes by using operations like GET, POST, DELETE.
+
+## Credits
+
+* My Classmates!
+
+## License
+
+* This project is licensed under MIT License.
+
+## Questions
+
+If you have any questions about this project, please contact me at 
+[aydenemateo@gmail.com](mailto:aydenemateo@gmail.com)
+* GitHub: [github.com/aydenemateo](https://github.com/aydenemateo/)
---
+++ routes/api/category-routes.js
@@ -0,0 +1,78 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500), json(err);
+    console.log(err)
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.update(
+      { category_name: req.body.category_name },
+      { where: { id: req.params.id, } }
+    );
+
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'no category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData) {
+      res.json(404).json({ message: 'No category found with this id!' })
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
